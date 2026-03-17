5f7d9171fe8b4b9fe6d8f149b114a6f87789692e
pocketbeast-hash1 <godlike.skills420@gmail.com>
2026-03-17 15:29:53 +0300

    add toasts

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,63 @@
 import 'package:caesar_zipher/app_logger.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import "telnet_client.dart";
+import "./widgets/toast_context.dart";
+
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
-    runApp(const MainApp());
+  runApp(
+    MaterialApp(
+      builder: FToastBuilder(),
+      home: MainApp(),
+      navigatorKey: navigatorKey,
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
-    const MainApp({super.key});
+  const MainApp({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            home: Scaffold(
-                body: Center(
-                    child: Row(
-                        children: [
-
-                            TextButton(
-                                onPressed: () async {
-                                    await TelnetClient.connect(TelnetConfig("192.168.2.153", 20000, "DataDM"));
-                                }, 
-                                child: Text("CONNECT TO PRINTER")
-                            ),
-                            TextButton(
-                                onPressed: () async {
-                                    await TelnetClient.disconnect();
-                                }, 
-                                child: Text("DISCONNECT FROM PRINTER")
-                            ),
-                            TextButton(
-                                onPressed: () async {
-                                    var response = await TelnetClient.sendCommand("GST");
-                                    AppLogger.logger.d("response from printer: $response");
-                                }, 
-                                child: Text("SEND COMMAND TO PRINTER")
-                            )
-
-                        ],
-                    ),
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ToastContext(
+        child: Center(
+          child: Row(
+            children: [
+              TextButton(
+                onPressed: () async {
+                  Future<void> promise = TelnetClient.connect(
+                    TelnetConfig("192.168.2.153", 20000, "DataDM"),
+                  );
+                  ToastContext.promise(promise, "pending", success: "success", error: "error");
+                },
+                child: Text("CONNECT TO PRINTER"),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await TelnetClient.disconnect();
+                },
+                child: Text("DISCONNECT FROM PRINTER"),
+              ),
+              TextButton(
+                onPressed: () async {
+                  var response = await TelnetClient.sendCommand("GST");
+                  AppLogger.logger.d("response from printer: $response");
+                },
+                child: Text("SEND COMMAND TO PRINTER"),
+              ),
+              TextButton(
+                onPressed: () {
+                  ToastContext.error("very very very very very very very very very very very very very very very very long text");
+                },
+                child: Text("show toast"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/widgets/toast_context.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import "package:caesar_zipher/main.dart";
+
+late FToast _fToast;
+
+class ToastContext extends StatefulWidget {
+  const ToastContext({super.key, required this.child});
+  final Widget child;
+
+  @override
+  State<ToastContext> createState() => _ToastContextState();
+
+  static void success(String text) {
+    _fToast.showToast(
+      child: _ToastContainer(
+        backgroundColor: Colors.greenAccent,
+        icon: Icon(Icons.check),
+        text: text,
+      ),
+    );
+  }
+
+  static void error(String text) {
+    _fToast.showToast(
+      child: _ToastContainer(
+        backgroundColor: const Color.fromARGB(255, 251, 134, 134),
+        icon: Icon(Icons.error_outline),
+        text: text,
+      ),
+    );
+  }
+
+  static void promise(
+    Future promise,
+    String pending, {
+    String? success,
+    String? error,
+  }) {
+    _fToast.showToast(
+      child: _ToastContainer(
+        backgroundColor: const Color.fromARGB(255, 180, 228, 253),
+        icon: Icon(Icons.pending_outlined),
+        text: pending,
+      ),
+      toastDuration: Duration(seconds: 60),
+    );
+
+    String finalState = "pending";
+    promise
+        .then((val) { finalState = "success"; })
+        .onError((err, trace) { finalState = "error"; })
+        .whenComplete(() {
+          _fToast.removeCustomToast();
+          if (finalState == "success" && success != null) {
+            ToastContext.success(success);
+          } else if (finalState == "error" && error != null) {
+            ToastContext.error(error);
+          }
+        });
+  }
+}
+
+class _ToastContextState extends State<ToastContext> {
+  @override
+  void initState() {
+    super.initState();
+    _fToast = FToast();
+    _fToast.init(navigatorKey.currentContext!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
+
+class _ToastContainer extends StatelessWidget {
+  const _ToastContainer({
+    required this.backgroundColor,
+    required this.icon,
+    required this.text,
+  });
+
+  final Color backgroundColor;
+  final Icon icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        color: backgroundColor,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [icon, SizedBox(width: 12.0), Text(text)],
+      ),
+    );
+  }
+}
