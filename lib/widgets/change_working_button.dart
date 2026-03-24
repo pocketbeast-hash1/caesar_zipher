162e60dff21b26d82d73fdbb64350c34f4aab81a
pocketbeast-hash1 <godlike.skills420@gmail.com>
2026-03-24 12:11:06 +0300

    add job updating on start

--- lib/widgets/change_working_button.dart
+++ lib/widgets/change_working_button.dart
@@ -1,4 +1,8 @@
+import 'package:caesar_zipher/app_logger.dart';
 import 'package:caesar_zipher/models/global_state_model.dart';
+import 'package:caesar_zipher/telnet_client.dart';
+import 'package:caesar_zipher/utils/queue.dart';
+import 'package:caesar_zipher/utils/settings.dart';
 import 'package:caesar_zipher/widgets/bool_button.dart';
 import 'package:caesar_zipher/widgets/toast_context.dart';
 import 'package:flutter/material.dart';
@@ -7,10 +11,30 @@ import 'package:provider/provider.dart';
 class ChangeWorkingButton extends StatelessWidget {
   const ChangeWorkingButton({super.key});
 
-  void _changeWorking(GlobalStateModel state, bool val) {
+  Future<void> _updateJob() async {
+    try {
+      Settings settings = await Settings.getSettings();
+      List<String> codes = await Queue.getQueue();
+      String code = codes.last;
+
+      Map<String, String> newFields = {settings.barcodeFieldName: code};
+      await TelnetClient.updateJob(newFields);
+      
+    } catch (e, s) {
+      AppLogger.logger.e("Ошибка при обновлении задания: $e, $s");
+    }
+  }
+
+  Future<void> _changeWorking(GlobalStateModel state, bool val) async {
     if (val && !state.printerConnected) {
-      ToastContext.error("Невозможно установить рабочий режим без подключения к принтеру!");
+      ToastContext.error(
+        "Невозможно установить рабочий режим без подключения к принтеру!",
+      );
       return;
+    }
+
+    if (val) {
+      await _updateJob();
     }
 
     state.setWorking(val);
@@ -22,9 +46,7 @@ class ChangeWorkingButton extends StatelessWidget {
       builder: (context, state, child) {
         return BoolButton(
           btnState: state.working,
-          text: state.working
-              ? "РАБОТА"
-              : "ОСТАНОВЛЕН",
+          text: state.working ? "РАБОТА" : "ОСТАНОВЛЕН",
           onPress: () => _changeWorking(state, !state.working),
         );
       },
